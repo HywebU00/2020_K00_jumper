d67a7083435ceee30e460bdd83bab1659059a6b7
Miley <miley.ho@hyweb.com.tw>
2021-07-06 18:28:28 +0800

    新增換色btn

---
+++ js/changecolor.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('body').append('<div class="changeColor"><ul><li><a href="#" class="initial_btn">原</a></li><li><a href="#" class="pu_btn">pu</a></li><li><a href="#" class="NCL_btn">NCL</a></li></ul></div>');
+
+    $('head').append("<link>");
+        css = $("head").children(":last");
+        css.attr({
+            rel: "stylesheet",
+            type: "text/css",
+            href: "",
+            id: "cssStyle"
+     });
+
+    $('.initial_btn').click(function(e) {
+    	$('.wrapper').removeClass('NCL pu');
+    });
+
+    $('.pu_btn').click(function(e) {
+        $('.wrapper').removeClass('NCL').addClass('pu');
+        $('head').find('#cssStyle').attr('href',"css/pu.css");
+        $('h1 img').attr('src','images/pu/logo_version.png');
+        $('.mp_logo img').attr('src','images/pu/logo.png');
+    });
+
+    $('.NCL_btn').click(function(e) {
+        $('.wrapper').removeClass('pu').addClass('NCL');
+        $('head').find('#cssStyle').attr('href',"css/NCL.css");
+        $('h1 img').attr('src','images/NCL/logo_version.png');
+        $('.mp_logo img').attr('src','images/NCL/logo.png');
+    });
+});
